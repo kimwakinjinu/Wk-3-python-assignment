7abfcb1c865ef094aa2b01fe7b148a6ef3ed1c00
Davidpraise Njinu <davidpraisenjinu@outlook.com>
2025-05-14 20:50:32 +0300

    Assignment done

---
+++ wk3.py
@@ -0,0 +1,13 @@
+my_list=[]
+list2=[50,60,70]
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+my_list.insert(1,15)
+my_list.extend(list2)
+my_list.pop()
+my_list.sort()
+print (my_list.index(30))
